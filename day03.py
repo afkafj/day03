ecf31339ab0156bf4a15d92397df7c97ddc6a42e
K <kk0412368@gmail.com>
2023-01-16 15:23:38 +0900

    8

--- day03.py
+++ day03.py
@@ -1,76 +1,11 @@
-# univ = 'inha university'
-# print(univ.split('i'))
-#
-#
-# print(univ[5:])
-# print(univ[5:15])
-# print(univ[-10:])
-# print(univ[::2])
-# print(univ[-3:])
-# print(univ[5:-6])
-# print(univ[-10:-6])
-#
-# print(len(univ))
-# #
-# #
-# # pokemon = ['피카츄', '꼬부기', '이상해', '파이리']
-# # pokemons_string = '%'.join(pokemon)
-# # print(pokemons_string)
-# #
-# # inha = 'a duck goes into a sea'
-# # print(inha.replace('a ', 'a famous '))
-# #
-# # subjects = ' ! python, data structure, database     !!!'
-# # print(subjects.strip('!'))
-# #
-# #
-# # print(subjects.find('data'), subjects.index('data'))
-# # print(subjects.rfind('data'))
-# # print(subjects.rindex('data'))
-# # print(subjects.count('data'))
-# # print(subjects.title())
-#
-#
-# song= '"when an eel grabs your arm, and it causes great harm, That s a moray"'
-#
-# # idx=song.rfind('m')
-# # song.replace(song[idx], song[idx].upper())
-# # print(song.endswith('moray!'))
-#
-# song_list = song.split()
-# print(song_list)
-# song_list[14] = song_list[14].title()
-# song_string = ''.join(song_list)
-# print(song_string)
-#
-#
-# # index = song.rfind('m')
-# # print(idx, song[idx].upper())
 
+# start_end = input("start and end number : ").split()
+# print(start_end)
+# print(int(start_end[0]), int(start_end[1]))
 
-# while True:
-#     dan = int(input('Dan (0 to quit): '))
-#
-#     if dan == 0:  # exit
-#         break
-#     if 1 < dan < 10:  # if dan >= 2 and dan <= 9:
-#         for i in range(1, 10):
-#             print('{0} * {1} = {2}'.format(dan, i, dan * i))
-#     else:
-#         print('2에서 9사이의 값을 입력 하세요')
-
-
-
-number = int(input("input number : "))
-is_prime = True
-
-for k in range(2, number):
-    if number % k == 0:
-        is_prime = False
-        break
-    print(k)
-
-if is_prime:
-    print(f'{number} is prime number!')
-else:
-    print(f'{number} is NOT prime number.')
+start = int(input("start number : "))
+end = int(input("end number : "))
+print(start, end)
+for k in range(start, end+1):
+    if
+    print(k,end=' ')
