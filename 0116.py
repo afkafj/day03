2ca7dcbd4bbe62dfde487d9119cd10f535f46b1b
K <kk0412368@gmail.com>
2023-01-16 17:02:14 +0900

    3

--- 0116.py
+++ 0116.py
@@ -3,10 +3,6 @@ duck = 'ducky McDuckface'
 gourd = 'McGourdface'
 Spitz = 'Spitzy Mcface'
 
-a = '%s' % duck
-b = '%s' % gourd
-c = '%s' % Spitz
-
-print(a)
-print(b)
-print(c)
+print(f'{duck}')
+print(f'{gourd}')
+print(f'{Spitz}')
