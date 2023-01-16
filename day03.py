0ed2c84b187613f3eb585363bc43e327d153a13c
K <kk0412368@gmail.com>
2023-01-16 14:24:32 +0900

    4

--- day03.py
+++ day03.py
@@ -11,38 +11,61 @@
 # print(univ[-10:-6])
 #
 # print(len(univ))
+# #
+# #
+# # pokemon = ['피카츄', '꼬부기', '이상해', '파이리']
+# # pokemons_string = '%'.join(pokemon)
+# # print(pokemons_string)
+# #
+# # inha = 'a duck goes into a sea'
+# # print(inha.replace('a ', 'a famous '))
+# #
+# # subjects = ' ! python, data structure, database     !!!'
+# # print(subjects.strip('!'))
+# #
+# #
+# # print(subjects.find('data'), subjects.index('data'))
+# # print(subjects.rfind('data'))
+# # print(subjects.rindex('data'))
+# # print(subjects.count('data'))
+# # print(subjects.title())
 #
 #
-# pokemon = ['피카츄', '꼬부기', '이상해', '파이리']
-# pokemons_string = '%'.join(pokemon)
-# print(pokemons_string)
+# song= '"when an eel grabs your arm, and it causes great harm, That s a moray"'
 #
-# inha = 'a duck goes into a sea'
-# print(inha.replace('a ', 'a famous '))
+# # idx=song.rfind('m')
+# # song.replace(song[idx], song[idx].upper())
+# # print(song.endswith('moray!'))
 #
-# subjects = ' ! python, data structure, database     !!!'
-# print(subjects.strip('!'))
+# song_list = song.split()
+# print(song_list)
+# song_list[14] = song_list[14].title()
+# song_string = ''.join(song_list)
+# print(song_string)
 #
 #
-# print(subjects.find('data'), subjects.index('data'))
-# print(subjects.rfind('data'))
-# print(subjects.rindex('data'))
-# print(subjects.count('data'))
-# print(subjects.title())
+# # index = song.rfind('m')
+# # print(idx, song[idx].upper())
 
 
-song= '"when an eel grabs your arm, and it causes great harm, That s a moray"'
+# while True:
+#     dan = int(input('Dan (0 to quit): '))
+#
+#     if dan == 0:  # exit
+#         break
+#     if 1 < dan < 10:  # if dan >= 2 and dan <= 9:
+#         for i in range(1, 10):
+#             print('{0} * {1} = {2}'.format(dan, i, dan * i))
+#     else:
+#         print('2에서 9사이의 값을 입력 하세요')
 
-# idx=song.rfind('m')
-# song.replace(song[idx], song[idx].upper())
-# print(song.endswith('moray!'))
-
-song_list = song.split()
-print(song_list)
-song_list[14] = song_list[14].title()
-song_string = ''.join(song_list)
-print(song_string)
 
 
-# index = song.rfind('m')
-# print(idx, song[idx].upper())
+number = int(input("정수 입력 : "))
+counts = 0
+
+k = 1
+while k <= 10:
+    if number % k == 0
+        counts= counts + 1
+
