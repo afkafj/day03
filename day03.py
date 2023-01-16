8676b08c7ef1417f9755330db0744a3b658af933
K <kk0412368@gmail.com>
2023-01-16 15:35:18 +0900

    찐찐막

--- day03.py
+++ day03.py
@@ -3,9 +3,24 @@
 # print(start_end)
 # print(int(start_end[0]), int(start_end[1]))
 
-start = int(input("start number : "))
-end = int(input("end number : "))
-print(start, end)
-for k in range(start, end+1):
-    if
-    print(k,end=' ')
+start = int(input("input start number : "))
+end = int(input("input end number : "))
+
+if end < start:
+    start, end = end, start
+
+for i in range(start, end+1):
+        # is_prime = True
+
+    if i <= 1:
+        continue
+    for k in range(2, i):
+        if i % k == 0:
+           # is_prime = False
+            break
+    else:
+        print(i, end=' ')
+
+
+
+
